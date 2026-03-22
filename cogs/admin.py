5db6fa151a5cc35e7d1099d7e221718500f5d982
feltaku <estakkun@gmail.com>
2026-03-22 16:13:36 +0900

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,16 +1,15 @@
 
 import discord
-from discord import app_commands
 from discord.ext import commands
 
 class admincog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="logoff", description="botを終了する")
-    async def logoff(self, interaction: discord.Interaction):
-        await interaction.response.send_message("botを停止します", ephemeral=True)
+    @commands.slash_command(name="logoff", description="botを終了する")
+    async def logoff(self, ctx):
+        await ctx.respond("botを停止します", ephemeral=True)
         await self.bot.close()
 
-async def setup(bot):
-    await bot.add_cog(admincog(bot))
+def setup(bot):
+    bot.add_cog(admincog(bot))
